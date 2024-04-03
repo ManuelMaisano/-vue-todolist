e97b19e8792cc7a865d246e5e96e3bd13b1696e4
Manuel Maisano <manumaisano@gmail.com>
2024-04-03 13:13:12 +0200

    data/script

---
+++ js/data.js
@@ -0,0 +1,37 @@
+export const padel = [
+    {
+        id: 1,
+        racket: 'Babolat',
+        done: false,
+    },
+    {
+        id: 2,
+        racket: 'StarVie',
+        done: false,
+    },
+    {
+        id: 3,
+        racket: 'Dropshot',
+        done: false,
+    },
+    {
+        id: 4,
+        racket: 'Varillon',
+        done: false,
+    },
+    {
+        id: 5,
+        racket: 'Wilson',
+        done: false,
+    },
+    {
+        id: 6,
+        racket: 'Adidas',
+        done: false,
+    },
+    {
+        id: 7,
+        racket: 'Siux',
+        done: false,
+    },
+]
---
+++ js/script.js
@@ -0,0 +1,24 @@
+import { padel } from './data.js';
+
+const { createApp } = Vue;
+
+
+
+
+createApp({
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+
+  },
+  mounted() {
+
+
+
+  },
+
+}).mount('#app')
